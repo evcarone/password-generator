720e128bb746595a0f6697697ca6d25dce937dff
E <evcarone@gmail.com>
2019-10-08 13:21:36 -0700

    added functions for getting type from user

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var getLength = prompt("Select Length of 8-128 characters: ");
-var isCharTypeNum = confirm("Include Numbers? ");
-var isCharTypeUpper = confirm("Include Upper Case? ");
-var isCharTypeLower = confirm("Include Lower Case? ");
-var isCharTypeSpecial = confirm("Include Special Characters? ");
+var getLength = 0;
+var isCharTypeNum = "false";
+var isCharTypeUpper = "false";
+var isCharTypeLower = "false";
+var isCharTypeSpecial = "false";
 var newPassword = [];
 var generatePasswordBtnEl = document.querySelector("#generatePassword")
 var displayPasswordEl = document.querySelector("#displayPassword");
@@ -12,24 +12,37 @@ const typeUpperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
 const typeNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const typeSpecialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', ':', '"', '<', '>', '?', '"'];
 
-//dynamically set the choices array based on user input for different character types
-//need to create a function that is passed to generateNewPassword -- so that user clicks button first
-
-var choicesArray = [];
-if (isCharTypeUpper)
-    choicesArray = choicesArray.concat(typeUpperCaseLetters);
-if (isCharTypeLower)
-    choicesArray = choicesArray.concat(typeUpperCaseLetters);
-if (isCharTypeNum)
-    choicesArray = choicesArray.concat(typeNumbers);
-if (isCharTypeSpecial)
-    choicesArray = choicesArray.concat(typeSpecialCharacters);
-
 generatePasswordBtnEl.addEventListener("click", function () {
     event.preventDefault();
+    getUserInput();
+    generateChoicesArray();
     generateNewPassword();
-
+    choicesArray = [];
 });
+
+//get user input
+function getUserInput() {
+    getLength = prompt("Select Length of 8-128 characters: ");
+    isCharTypeNum = confirm("Include Numbers? ");
+    isCharTypeUpper = confirm("Include Upper Case? ");
+    isCharTypeLower = confirm("Include Lower Case? ");
+    isCharTypeSpecial = confirm("Include Special Characters? ");
+};
+
+//dynamically set the choices array based on user input for different character types
+//initialize choicesArray and then dynamically create the based on the user choices
+var choicesArray = [];
+
+function generateChoicesArray() {
+    if (isCharTypeUpper == true)
+        choicesArray = choicesArray.concat(typeUpperCaseLetters);
+    if (isCharTypeLower == true)
+        choicesArray = choicesArray.concat(typeUpperCaseLetters);
+    if (isCharTypeNum == true)
+        choicesArray = choicesArray.concat(typeNumbers);
+    if (isCharTypeSpecial == true)
+        choicesArray = choicesArray.concat(typeSpecialCharacters);
+};
 
 //function to generate the new passowrd based on dynamically constructing an array
 //need to add logic for better user experience on error for length or type choices
@@ -37,11 +50,8 @@ function generateNewPassword() {
     if (choicesArray.length < typeNumbers.length || getLength < 8) {
         alert("You did not meet the minimum standard ")
     } else {
-        console.log("get length value : " + getLength);
         for (i = 0; i < getLength; i++) {
-            console.log("math random new value : " + Math.floor(Math.random() * (choicesArray.length - 1)));
             newPassword += choicesArray[Math.floor(Math.random() * (choicesArray.length - 1))];
-            console.log("log array value for new password : " + newPassword[i])
         }
         displayPasswordEl.textContent = newPassword;
     };
